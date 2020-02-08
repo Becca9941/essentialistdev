f288e5519ab3d1b5369be44d53030c0ec9a40658
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-02-08 11:37:52 +1100

    ➕ Add material Fab button.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // Import
 // Top App Bar
 import {MDCTopAppBar} from '@material/top-app-bar';
+// Ripple
+import {MDCRipple} from '@material/ripple';
+
 
 // Instantiate
 // Top App Bar
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+// Fab
+const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
