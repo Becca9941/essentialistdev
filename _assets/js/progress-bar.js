1aacd7c84de57df85bc2f9815604fe74577c8190
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-02-28 15:12:38 +1100

    🚧 Auto render posts that are added to the post dir.

--- _assets/js/progress-bar.js
+++
@@ -1,9 +0,0 @@
-// Progress Bar
-window.onscroll = function() { progressBar() };
-
-function progressBar() {
-  const distanceFromTop = document.body.scrollTop || document.documentElement.scrollTop;
-  const pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const progressBarWidth = (distanceFromTop / pageHeight) * 100;
-  document.querySelector('.bar').style.width = progressBarWidth + '%';
-}
