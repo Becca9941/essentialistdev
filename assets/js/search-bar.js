eed3c98f1dc860ebb5695a941e6d5f0a27a8c961
Rebecca Williams <rebeccawilliams9941@gmail.com>
2020-03-15 19:37:38 +1100

    ✨ Finish fully functional search bar woo!

---
+++ assets/js/search-bar.js
@@ -0,0 +1,9 @@
+function openSearch() {
+  const searchBar = document.getElementById("search-container");
+  searchBar.classList.add("open");
+}
+
+function closeSearch() {
+  const searchBar = document.getElementById("search-container");
+  searchBar.classList.remove("open");
+}
